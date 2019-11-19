1a5e5c87bb3f3798e842d7912292939489508a80
Ted Kordas <Ted@Teds-MacBook-Air.local>
2019-11-18 17:09:44 -0700

    Updated App

--- src/App.js
+++ src/App.js
@@ -6,18 +6,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <form action="/" method="get"></form>
+        <div>
+          <label for="firstName">First Name</label>
+          <input id="firstName" name="firstName" type="text"></input>
+        </div>
+        <div>
+          <label for="lastName">Last Name</label>
+          <input id="lastName" name="lastName" type="text"></input>
+        </div>
+        <div>
+          <label for="coach">Coach</label>
+          <input id="coach" name="coach" type="text"></input>
+        </div>
+        <div>
+          <label for="sledType">Sled Type</label>
+          <input id="sledType" name="sledType" type="text"></input>
+        </div>
+        <div>
+          <label for="date">Date</label>
+          <input id="date" name="date" type="date"></input>
+        </div>
+        <div>
+          <label for="time">Time</label>
+          <input id="time" name="time" type="time"></input>
+        </div>
       </header>
     </div>
   );
