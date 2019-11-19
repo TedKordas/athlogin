d1a06881f0b9f2ec47d69ef0f03ba1bbaa609251
Ted Kordas <Ted@Teds-MacBook-Air.local>
2019-11-18 17:28:08 -0700

    Required and submit button changed

--- src/App.js
+++ src/App.js
@@ -6,33 +6,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <form action="/" method="get"></form>
+      <form id="form" action="/" method="GET"></form>
         <div>
           <label for="firstName">First Name</label>
-          <input id="firstName" name="firstName" type="text"></input>
+          <input id="firstName" name="firstName" type="text" required></input>
         </div>
         <div>
           <label for="lastName">Last Name</label>
-          <input id="lastName" name="lastName" type="text"></input>
+          <input id="lastName" name="lastName" type="text" required></input>
         </div>
         <div>
           <label for="coach">Coach</label>
-          <input id="coach" name="coach" type="text"></input>
+          <input id="coach" name="coach" type="text" required></input>
         </div>
         <div>
           <label for="sledType">Sled Type</label>
-          <input id="sledType" name="sledType" type="text"></input>
+          <input id="sledType" name="sledType" type="text" required></input>
         </div>
         <div>
           <label for="date">Date</label>
-          <input id="date" name="date" type="date"></input>
+          <input id="date" name="date" type="date" required></input>
         </div>
         <div>
           <label for="time">Time</label>
-          <input id="time" name="time" type="time"></input>
+          <input id="time" name="time" type="time" required></input>
         </div>
         <div>
-          <input id="submit" name="submit" type="submit"></input>
+          <button type="submit">Submit</button>
         </div>
       </header>
     </div>
