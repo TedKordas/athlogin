d395d65830951a7c57325b7b32d7c0eaf06f0175
Ted Kordas <Ted@Teds-MacBook-Air.local>
2019-11-19 10:37:36 -0700

    Fixing posts

--- src/App.js
+++ src/App.js
@@ -5,35 +5,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div id="error"></div>
-      <form id="form" method="POST" action="/startList"></form>
-        <div>
-          <label for="firstName">First Name:</label>
-          <input id="firstName" name="firstName" type="text" required></input>
-        </div>
-        <div>
-          <label for="lastName">Last Name:</label>
-          <input id="lastName" name="lastName" type="text" required></input>
-        </div>
-        <div>
-          <label for="coach">Coach:</label>
-          <input id="coach" name="coach" type="text" required></input>
-        </div>
-        <div>
-          <label for="sledType">Sled Type</label>
-          <input id="sledType" name="sledType" type="text" required></input>
-        </div>
-        <div>
-          <label for="date">Date:</label>
-          <input id="date" name="date" type="date" required></input>
-        </div>
-        <div>
-          <label for="time">Time:</label>
-          <input id="time" name="time" type="time" required></input>
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
+        <form id="form" method="POST" action="/">
+          <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input id="firstName" name="firstName" type="text" required></input>
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input id="lastName" name="lastName" type="text" required></input>
+          </div>
+          <div>
+            <label htmlFor="coach">Coach:</label>
+            <input id="coach" name="coach" type="text" required></input>
+          </div>
+          <div>
+            <select>
+              <option value="bobsleigh">Bobsleigh</option>
+              <option value="skeleton">Skeleton</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="date">Date:</label>
+            <input id="date" name="date" type="date" required></input>
+          </div>
+          <div>
+            <label htmlFor="time">Time:</label>
+            <input id="time" name="time" type="time" required></input>
+          </div>
+          <div>
+            <label htmlFor="runs">Runs:</label>
+            <input id="runs" name="runs" type="number" min="1" max="10" required></input>
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </header>
     </div>
   );
--- src/posting.js
+++ src/posting.js
@@ -2,15 +2,24 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+const PORT = 3000;
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('form');
+    res.sendFile(__dirname + '/App.js');
+    res.sendFile("App.js");
 });
 
-app.post('/startList', urlencodedParser, function(req, res) {
+app.post('/', urlencodedParser, function(req, res) {
+    var fistName = req.body.firstName;
     console.log(req.body);
     res.render('contact', {qs: req.query});
 });
+
+app.listen(`${PORT}`)
 
